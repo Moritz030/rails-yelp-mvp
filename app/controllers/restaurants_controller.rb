d73beadacfe2a4368f66bdea84f762cf83bb2a7a
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-04-13 20:57:05 +0200

    create action and new action/view for restaurant added, some styling done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,8 +8,25 @@ class RestaurantsController < ApplicationController
   def show
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(get_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
   private
   def find_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+  private
+  def get_params
+    params.require(:restaurant).permit(:name, :phone_number, :address, :category)
   end
 end
